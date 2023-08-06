e0814d7934ff6631985c56227da8a4463d208bea
Siddharth <siddharth99c@gmail.com>
2023-08-06 20:41:54 +0530

    feat: windows api test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-fn main() {
-    let devices = ffi::get_aperture_devices();
-    println!("Devices: {}", devices);
+use windows::{
+    core::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
+    Win32::UI::WindowsAndMessaging::*,
+};
+
+fn main() -> Result<()> {
+    #[cfg(target_os = "macos")]
+    {
+        let devices = ffi::get_aperture_devices();
+        println!("Devices: {}", devices);
+    }
+
+    println!("Hello, world!");
+    let doc = XmlDocument::new()?;
+    doc.LoadXml(h!("<html>hello world</html>"))?;
+
+    let root = doc.DocumentElement()?;
+    assert!(root.NodeName()? == "html");
+    assert!(root.InnerText()? == "hello world");
+
+    unsafe {
+        let event = CreateEventW(None, true, false, None)?;
+        SetEvent(event).ok()?;
+        WaitForSingleObject(event, 0);
+        CloseHandle(event).ok()?;
+
+        MessageBoxA(None, s!("Ansi"), s!("Caption"), MB_OK);
+        MessageBoxW(None, w!("Wide"), w!("Caption"), MB_OK);
+    }
+
+    Ok(())
 }
 
+#[cfg(target_os = "macos")]
 #[swift_bridge::bridge]
 mod ffi {
     // extern "Rust" {
