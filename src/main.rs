f79784940c06decd657c97c15fc2a9c07f385087
Siddharth <siddharth99c@gmail.com>
2023-09-27 02:04:45 +0530

    feat: ffmpeg

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use windows::{
     Win32::UI::WindowsAndMessaging::*,
 };
 
+extern crate ffmpeg_next;
+
 fn main() -> Result<(), ()> {
     #[cfg(target_os = "macos")]
     {
@@ -11,44 +13,16 @@ fn main() -> Result<(), ()> {
         println!("Devices: {}", devices);
     }
 
-    println!("Hello, world!");
+    let ffmpeg = ffmpeg_next::init().unwrap();
 
-    #[cfg(target_os = "windows")]
-    {
-        let doc = XmlDocument::new()?;
-        doc.LoadXml(h!("<html>hello world</html>"))?;
-
-        let root = doc.DocumentElement()?;
-        assert!(root.NodeName()? == "html");
-        assert!(root.InnerText()? == "hello world");
-
-        unsafe {
-            let event = CreateEventW(None, true, false, None)?;
-            SetEvent(event).ok()?;
-            WaitForSingleObject(event, 0);
-            CloseHandle(event).ok()?;
-
-            MessageBoxA(None, s!("Ansi"), s!("Caption"), MB_OK);
-            MessageBoxW(None, w!("Wide"), w!("Caption"), MB_OK);
-        }
-    }
+    println!("{:?}", ffmpeg);
 
     Ok(())
 }
 
 #[swift_bridge::bridge]
 mod ffi {
-    // extern "Rust" {
-    //     fn rust_double_number(num: i64) -> i64;
-    // }
-
     extern "Swift" {
         fn get_aperture_devices() -> String;
     }
 }
-
-// fn rust_double_number(num: i64) -> i64 {
-//     println!("Rust double function called...");
-
-//     num * 2
-// }
