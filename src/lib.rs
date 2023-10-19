0d6242f21332a8879c50ebd42a3c752898b0c864
Siddharth <siddharth99c@gmail.com>
2023-10-19 23:32:14 +0530

    feat: adds windows-capture crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 #[cfg(target_os = "macos")]
 mod mac;
 
@@ -12,6 +14,7 @@ pub enum TargetKind {
 
 pub struct Target {
     kind: TargetKind,
+    name: String,
     id: u32,
 }
 
@@ -22,30 +25,58 @@ pub struct Options {
     show_highlight: bool,
 }
 
-pub fn capture() {
-    #[cfg(target_os = "macos")]
-    mac::main();
-
-    #[cfg(target_os = "windows")]
-    win::main();
+pub struct Recorder {
+    id: String,
 }
 
-pub fn is_supported() -> bool {
+impl Recorder {
+    pub fn init() -> Self {
+        Recorder {
+            id: String::from(""),
+        }
+    }
+
+    pub fn start_capture(&self, options: Options) {
+        let mut rng = rand::thread_rng();
+        let id: u32 = rng.gen();
+        println!("id: {}", id);
+
+        #[cfg(target_os = "macos")]
+        mac::main();
+
+        #[cfg(target_os = "windows")]
+        win::main();
+    }
+
+    pub fn stop_capture() {
+        #[cfg(target_os = "macos")]
+        mac::stop_capture();
+
+        // #[cfg(target_os = "windows")]
+        // win::stop_capture();
+    }
+
+    pub fn is_supported() -> bool {
+        #[cfg(target_os = "macos")]
+        let access = mac::is_supported();
+
+        #[cfg(target_os = "windows")]
+        let access = win::is_supported();
+
+        access
+    }
+
+    pub fn get_targets() -> Vec<Target> {
+        #[cfg(target_os = "macos")]
+        let targets = mac::get_targets();
+
+        #[cfg(target_os = "windows")]
+        let targets = win::get_targets();
+        targets
+    }
+
     #[cfg(target_os = "macos")]
-    let access = mac::is_supported();
-
-    #[cfg(target_os = "windows")]
-    let access = win::is_supported();
-
-    access
-}
-
-pub fn has_permission() -> bool {
-    #[cfg(target_os = "macos")]
-    let permission = mac::has_permission();
-
-    #[cfg(target_os = "windows")]
-    let permission = true; // TODO: check Windows permissions
-
-    permission
+    pub fn has_permission() -> bool {
+        mac::has_permission()
+    }
 }
