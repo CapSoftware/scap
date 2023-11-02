10f2605bd4cb593eb7d36389a8eb656ff3d3f5bf
Siddharth <siddharth99c@gmail.com>
2023-11-02 21:41:49 +0530

    refactor: moves temp functions to diff file

--- src/mac/mod.rs
+++ src/mac/mod.rs
@@ -1,11 +1,8 @@
-use core_graphics::access::ScreenCaptureAccess;
-use core_graphics::display::{CGDirectDisplayID, CGDisplay, CGMainDisplayID};
-use core_video_sys::{
-    CVPixelBufferGetBaseAddressOfPlane, CVPixelBufferGetBytesPerRowOfPlane,
-    CVPixelBufferGetHeightOfPlane, CVPixelBufferGetWidthOfPlane, CVPixelBufferLockBaseAddress,
-    CVPixelBufferRef, CVPixelBufferUnlockBaseAddress,
+use core_graphics::{
+    access::ScreenCaptureAccess,
+    display::{CGDirectDisplayID, CGDisplay},
 };
-use screencapturekit::sc_display::SCDisplay;
+use core_video_sys::CVPixelBufferRef;
 use screencapturekit::{
     sc_content_filter::{InitParams, SCContentFilter},
     sc_error_handler,
@@ -19,6 +16,8 @@ use std::{path::PathBuf, process::Command};
 
 use crate::{Target, TargetKind};
 
+mod temp;
+
 struct ConsoleErrorHandler;
 
 impl sc_error_handler::StreamErrorHandler for ConsoleErrorHandler {
@@ -29,63 +28,12 @@ impl sc_error_handler::StreamErrorHandler for ConsoleErrorHandler {
 
 struct OutputHandler {}
 
-// Convert YCbCr to RGB
-fn ycbcr_to_rgb(
-    y_data: &[u8],
-    cbcr_data: &[u8],
-    width: usize,
-    height: usize,
-    stride: usize,
-) -> Vec<u8> {
-    let mut rgb_data = Vec::with_capacity(width * height * 3);
-    let row = width + stride;
-
-    for h in 0..height {
-        for w in 0..width {
-            let y_idx = h * row + w;
-            let uv_idx = (h / 2) * row + w - w % 2;
-
-            // let y = y_data[y_idx] as f32;
-            // let cb = cbcr_data[uv_idx] as f32 - 128.0;
-            // let cr = cbcr_data[uv_idx + 1] as f32 - 128.0;
-            let y = (y_data[y_idx] as f32 - 16.0) * (255.0 / (235.0 - 16.0));
-            let cb = (cbcr_data[uv_idx] as f32 - 16.0) * (255.0 / (240.0 - 16.0)) - 128.0;
-            let cr = (cbcr_data[uv_idx + 1] as f32 - 16.0) * (255.0 / (240.0 - 16.0)) - 128.0;
-
-            let r = (y + 1.402 * cr).max(0.0).min(255.0) as u8;
-            let g = (y - 0.344136 * cb - 0.714136 * cr).max(0.0).min(255.0) as u8;
-            let b = (y + 1.772 * cb).max(0.0).min(255.0) as u8;
-
-            rgb_data.push(r);
-            rgb_data.push(g);
-            rgb_data.push(b);
-        }
-    }
-    rgb_data
-}
-
 // Get the scale factor of given display
 fn get_scale_factor(display_id: CGDirectDisplayID) -> u64 {
     let mode = CGDisplay::new(display_id).display_mode().unwrap();
     let width = mode.width();
     let pixel_width = mode.pixel_width();
     pixel_width / width
-}
-
-// TEMP: get main display
-fn get_main_display() -> SCDisplay {
-    let content = SCShareableContent::current();
-    let displays = content.displays;
-
-    let main_display_id = unsafe { CGMainDisplayID() };
-    let main_display = displays
-        .iter()
-        .find(|display| display.display_id == main_display_id)
-        .unwrap_or_else(|| {
-            panic!("Main display not found");
-        });
-
-    main_display.to_owned()
 }
 
 impl StreamOutput for OutputHandler {
@@ -101,77 +49,17 @@ impl StreamOutput for OutputHandler {
                     _ => {
                         let ptr = sample.ptr;
                         let timestamp = ptr.get_presentation_timestamp().value;
-                        let pixel_buffer = ptr.get_image_buffer().get_raw() as CVPixelBufferRef;
+                        let buffer = ptr.get_image_buffer().get_raw() as CVPixelBufferRef;
 
-                        unsafe {
-                            // Lock the base address
-                            CVPixelBufferLockBaseAddress(pixel_buffer, 0);
+                        let (width, height, data) = unsafe { temp::get_data_from_buffer(buffer) };
 
-                            // Check the format of the pixel buffer
-                            // let format = core_video_sys::CVPixelBufferGetPixelFormatType(pixel_buffer);
+                        let img =
+                            image::RgbImage::from_raw(width as u32, height as u32, data).unwrap();
 
-                            // Currently: 875704438, kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange
-                            // TODO: Capture in BRGA format instead
-
-                            // Plane 1 — Y (Luma)
-                            let y_width = CVPixelBufferGetWidthOfPlane(pixel_buffer, 0);
-                            let y_height = CVPixelBufferGetHeightOfPlane(pixel_buffer, 0);
-                            let y_bytes_row = CVPixelBufferGetBytesPerRowOfPlane(pixel_buffer, 0);
-                            let y_address = CVPixelBufferGetBaseAddressOfPlane(pixel_buffer, 0);
-                            let y_stride = y_bytes_row - y_width;
-
-                            // Plane 2 — CbCr (Chroma)
-                            let c_width = CVPixelBufferGetWidthOfPlane(pixel_buffer, 1);
-                            let c_height = CVPixelBufferGetHeightOfPlane(pixel_buffer, 1);
-                            let c_address = CVPixelBufferGetBaseAddressOfPlane(pixel_buffer, 1);
-                            let c_bytes_row = CVPixelBufferGetBytesPerRowOfPlane(pixel_buffer, 1);
-                            let c_stride = c_bytes_row - c_width;
-
-                            let y_data = std::slice::from_raw_parts(
-                                y_address as *const u8,
-                                y_height as usize * y_bytes_row as usize,
-                            );
-
-                            let c_data = std::slice::from_raw_parts(
-                                c_address as *const u8,
-                                c_height as usize * c_bytes_row as usize,
-                            );
-
-                            // Logs
-                            println!("y_width: {:?}", y_width);
-                            println!("y_height: {:?}", y_height);
-                            println!("y_address: {:?}", y_address);
-                            println!("y_bytes_per_row: {:?}", y_bytes_row);
-                            println!("c_width: {:?}", c_width);
-                            println!("c_height: {:?}", c_height);
-                            println!("c_address: {:?}", c_address);
-                            println!("c_bytes_per_row: {:?}", c_bytes_row);
-
-                            // println!("y_data: {:?}", y_data);
-                            // println!("c_data: {:?}", c_data);
-
-                            // Convert YUV buffer to RGB
-                            let rgb_data =
-                                ycbcr_to_rgb(&y_data, &c_data, y_width, y_height, y_stride);
-
-                            let img = image::RgbImage::from_raw(
-                                y_width as u32,
-                                y_height as u32,
-                                rgb_data,
-                            )
-                            .unwrap();
-
-                            // Save the image to disk
-                            let filename = format!("frame_{}.png", timestamp);
-                            // println!("rgb_data: {:?}", &rgb_data[0..5]);
-                            println!("Captured frame: {}", filename);
-
-                            let folder = PathBuf::new().join("test").join(filename);
-                            img.save(folder).expect("Failed to save image");
-
-                            // unlock base address
-                            CVPixelBufferUnlockBaseAddress(pixel_buffer, 0);
-                        }
+                        // Save the image to disk
+                        let filename = format!("frame_{}.png", timestamp);
+                        let folder = PathBuf::new().join("test").join(filename);
+                        img.save(folder).expect("Failed to save image");
                     }
                 }
             }
@@ -183,7 +71,7 @@ impl StreamOutput for OutputHandler {
 }
 
 pub fn main() {
-    let display = get_main_display();
+    let display = temp::get_main_display();
     let display_id = display.display_id;
 
     let scale_factor = get_scale_factor(display_id) as u32;
