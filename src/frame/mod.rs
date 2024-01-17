42d84c5c8e6780a9f2dfa6a7c33fbbf616a3bfb7
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-01-17 09:15:10 +0530

    feat: create func to remove alpha channel from image data

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -41,3 +41,17 @@ pub enum FrameData<'a> {
     BGR0(&'a [u8]),
 }
 
+pub fn remove_alpha_channel(frame_data: Vec<u8>) -> Vec<u8> {
+    let width = frame_data.len();
+    let width_without_alpha = (width / 4) * 3;
+
+    let mut data :Vec<u8> = vec![0; width_without_alpha];
+
+    for (src, dst) in frame_data.chunks_exact(4).zip(data.chunks_exact_mut(3)) {
+        dst[0] = src[0];
+        dst[1] = src[1];
+        dst[2] = src[2];
+    }
+
+    return data;
+}
